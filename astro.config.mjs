23e0aded4009dcbacd9f7e2e24f678233e0b8cc1
supernbk1ng <zst1873323741@gmail.com>
2026-01-25 21:02:38 +0800

    fix: add site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://supernbk1ng-supernbkingblog.vercel.app',
   adapter: vercel(),
   output: 'server',
   vite: {
